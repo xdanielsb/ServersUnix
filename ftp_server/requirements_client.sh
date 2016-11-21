a0b6e1bdf8210553af2d93620ceec35bbce61585
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-11-20 20:34:05 -0500

    Add the necesary configurations  to the server ftp, and add the requeriments for testing a client from the client side.

--- ftp_server/requirements_client.sh
+++ ftp_server/requirements_client.sh
@@ -3,3 +3,8 @@
 # the code.
 
 wget http://sourceforge.net/projects/filezilla/files/FileZilla_Client/3.22.2.2/FileZilla_3.22.2.2_x86_64-linux-gnu.tar.bz2/download?accel_key=57%3A1479689888%3Ahttps%253A//filezilla-project.org/download.php%253Ftype%253Dclient%3A72907993%24ab6d98f2e2d359bc494ae4666351fb61091b257f&click_id=913f027e-af85-11e6-9b1d-0200ac1d1d92&source=accel -O filezilla
+
+#as a recomendation run this code for see the users
+#cat /etc/ftpusers
+
+#test the connection as an anonymus user and a real user.
