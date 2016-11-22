e630ac9498fdbded4f020e6128d9a0029ac4f643
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-11-22 18:09:37 -0500

    End configuration of the proxy server, and add some info and some requirements for creating the proxy server

--- proxy_server/requeriments.sh
+++ proxy_server/requeriments.sh
@@ -1,6 +1,12 @@
 #!bin/bash
 # Install the following requeriments
 
+
+# sometimes the package is squid instead of squid
+# Just changes some additional feauters but the 
+# other thing are the same.
+
+
 sudo apt-get install squid3
 
 #optional, check the status squid
