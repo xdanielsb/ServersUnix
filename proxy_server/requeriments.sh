096c209b0840990abc2820882ff29b560ff4e37c
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-11-22 05:57:25 -0500

    Add initial requeriments for configuring proxy server.

---
+++ proxy_server/requeriments.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+# Install the following requeriments
+
+sudo apt-get install squid3
+
+#optional, check the status squid
+sudo service squid3 status
+
+#start the service 
+sudo service squid3 start 
