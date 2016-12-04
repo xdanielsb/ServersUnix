4252b186e44c27a19b1043559fbdb9178b6ab836
Daniel <dfsantosb@correo.udistrital.edu.co>
2016-12-04 11:10:30 -0500

    Instert requeriments for install on Fedora

--- ftp_server/requirements_server.sh
+++ ftp_server/requirements_server.sh
@@ -1,4 +1,13 @@
 #!bin/bash
+
+#Any distribution of Debian
 #This bash add the requirements for creating a ftp_server
 
 sudo apt-get install vsftpd
+
+#Distribution of fedora update our packages
+sudo dnf -y update 
+#install vsftp and any require packages 
+sudo dnf -y install vsftpd
+
+
